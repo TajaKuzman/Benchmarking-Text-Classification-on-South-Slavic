3445b2dbc5c74309ed9cb9a0e4a405c8b4589b17
Taja Kuzman Pungeršek <86299119+TajaKuzman@users.noreply.github.com>
2025-11-17 13:32:24 +0000

    Edited

--- Interactive-Dashboard/app.py
+++ Interactive-Dashboard/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import seaborn as sns
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 
 # ---------------------
 # Data loading
